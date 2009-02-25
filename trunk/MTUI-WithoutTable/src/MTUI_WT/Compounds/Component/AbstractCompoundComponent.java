33bcddbf315bd858f8e8ccc2511b4aebae4cd675
n.d.santos <n.d.santos@4d98e7b0-fc5f-11dd-99c0-75d572ba1ddd>
2009-02-25 19:54:28 +0000

    teclas brancas do piano

--- trunk/MTUI-WithoutTable/src/MTUI_WT/Compounds/Component/AbstractCompoundComponent.java
+++ trunk/MTUI-WithoutTable/src/MTUI_WT/Compounds/Component/AbstractCompoundComponent.java
@@ -2,6 +2,7 @@ package MTUI_WT.Compounds.Component;
 
 
 import java.awt.Dimension;
+import java.awt.Point;
 
 import MTUI_WT.Compounds.IMTCompoundControl;
 import MTUI_WT.Compounds.MTAbstractCompoundControl;
@@ -11,6 +12,8 @@ public abstract class AbstractCompoundComponent extends MTAbstractControl{
 
 	private static final long serialVersionUID = 1L;
 	private MTAbstractCompoundControl mParent;
+	private Dimension refSize;
+	private Point refLocation;
 	
 	public AbstractCompoundComponent(MTAbstractCompoundControl parent)
 	{
@@ -22,19 +25,49 @@ public abstract class AbstractCompoundComponent extends MTAbstractControl{
 		return this.mParent;
 	}
 	
-	
+	public Point getReferenceLocation(){
 		
+		return this.refLocation;
+	}
+	
+	public void setReferenceLocation(Point location){
+		
+		this.refLocation =location;
+	}
+	
+	public Dimension getReferenceSize(){
+		return this.refSize;
+	}
+	public void setReferenceSize(Dimension size){
+		this.refSize=size;
+	}
+	
+	public void UpdateSizeAndLocation(){
+		int newX = (int) (this.mParent.getX() + (this.getReferenceLocation().getX()*this.mParent.getWidth())/100);
+		int newY = (int) (this.mParent.getY() + (this.getReferenceLocation().getY()*this.mParent.getHeight())/100);
+		super.setLocation(newX, newY);
+		
+		int newWidth = (int) (this.mParent.getWidth()*this.getReferenceSize().getWidth()/100);
+		int newHeight = (int) (this.mParent.getHeight()*this.getReferenceSize().getHeight()/100);
+	
+		super.setSize(new Dimension(newWidth, newHeight));
+	}
+	
 	@Override
 	public void setLocation(int x, int y) {
-		int newX = this.mParent.getX() + (x*this.mParent.getWidth())/100;
-		int newY = this.mParent.getY() + (y*this.mParent.getHeight())/100;
+		this.setReferenceLocation(new Point(x,y));
+		
+		int newX = (int) (this.mParent.getX() + (this.getReferenceLocation().getX()*this.mParent.getWidth())/100);
+		int newY = (int) (this.mParent.getY() + (this.getReferenceLocation().getY()*this.mParent.getHeight())/100);
 		super.setLocation(newX, newY);
 	}
 	
 	@Override
 	public void setSize(Dimension d) {
-		int newWidth = (int) (this.mParent.getWidth()*d.getWidth()/100);
-		int newHeight = (int) (this.mParent.getHeight()*d.getHeight()/100);
+		this.setReferenceSize(d);
+		
+		int newWidth = (int) (this.mParent.getWidth()*this.getReferenceSize().getWidth()/100);
+		int newHeight = (int) (this.mParent.getHeight()*this.getReferenceSize().getHeight()/100);
 	
 		super.setSize(new Dimension(newWidth, newHeight));
 		
